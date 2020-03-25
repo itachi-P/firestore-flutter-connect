129b8e7cbf1cba450b50a311b2585f242512cce0
itachi-P <itachip38@gmail.com>
2020-03-25 23:57:31 +0900

    Document ID (sample-user_001) registered in cloudFirestore was successfully acquired.

--- lib/change_habit.dart
+++ lib/change_habit.dart
@@ -179,7 +179,11 @@ class _ChangeHabitState extends State<ChangeHabit> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               RaisedButton(
-                onPressed: _readFirestore(context),
+                onPressed: () {
+                  Stream<List> streamlist = _buildList(context);
+                  Future<List> list = streamlist.first;
+                  list.then((value) => print(value.first.document_id));
+                },
                 child: Text("読み込み"),
               ),
               RaisedButton(
@@ -194,8 +198,14 @@ class _ChangeHabitState extends State<ChangeHabit> {
   }
 
   // 通るが、ボタンが押下不能な状態になるのでコールバック関数が未だに理解できていない模様
-  _readFirestore(BuildContext context) {
-      print(Firestore.instance.collection('change-habit').snapshots().map((data) => data.toString()));
+  _buildList(BuildContext context) {
+    return Firestore.instance.collection('change-habit').snapshots().map((data) =>
+        data.documents.map((data) => _buildListItem(context, data)).toList());
+  }
+
+  _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+    return record;
   }
 
   Widget HabitCalendar() {
@@ -214,81 +224,19 @@ class _ChangeHabitState extends State<ChangeHabit> {
           ]),
     );
   }
+}
 
-//  Widget DairyRecord(int index) {
-//    return Container(
-//      width: 50.0,
-//      height: 50.0,
-//      // TODO タップされたマスを一意に区別してその色を変えるように変更
-//      key: ValueKey(index.toString()),
-//      color: _selectedItem == null ? Colors.grey[400] : Colors.red,
-//      child: InkWell(
-//        onTap: _onTapDairyRecord,
-//        child: Text(index.toString()),
-//      ),
-//    );
-//  }
-//
-//  Future<void> _onTapDairyRecord() async {
-//    print("onTap called.");
-//
-//    List<DropdownMenuItem<String>> getDropDownMenuItems() {
-//      List<DropdownMenuItem<String>> items = List();
-//      for (String item in _grading) {
-//        items.add(DropdownMenuItem(value: item, child: Text(item)));
-//      }
-//      return items;
-//    }
-//
-//    // 選択アイテム変更処理
-//    void changedDropDownItem(String selectedItem) {
-//      setState(() {
-//        _selectedItem = selectedItem;
-//        print("selected:" + _selectedItem);
-//      });
-//    }
-//
-//    // ドロップダウンメニュー入力フォーム
-//    DropdownButtonHideUnderline dropdownMenu() {
-//      return DropdownButtonHideUnderline(
-//        child: ButtonTheme(
-//          alignedDropdown: true,
-//          child: DropdownButton(
-//            key: Key('達成度'),
-//            value: _selectedItem,
-//            items: getDropDownMenuItems(),
-//            onChanged: changedDropDownItem,
-//          ),
-//        ),
-//      );
-//    }
-//
-//    return showDialog<void>(
-//      context: context,
-//      // [false] user must tap button. [true] user can tap outside dialog.
-//      barrierDismissible: false,
-//      builder: (BuildContext dialogContext) {
-//        return AlertDialog(
-//          title: Text('段階評価'),
-//          // TODO ここをプルダウンメニュー化し、選択したvalueによって色分けする。
-//          content: SingleChildScrollView(
-//            child: ListBody(
-//              children: <Widget>[
-//                dropdownMenu(),
-//              ],
-//            ),
-//          ),
-//          actions: <Widget>[
-//            FlatButton(
-//              child: Text('決定'),
-//              onPressed: () {
-//                Navigator.of(dialogContext)
-//                    .pop(_selectedItem); // Dismiss alert dialog
-//              },
-//            ),
-//          ],
-//        );
-//      },
-//    );
-//  }
+class Record {
+  final String document_id;
+  //final String full_name;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(reference.documentID != null),
+        //assert(map['full_name'] != null),
+        document_id = reference.documentID;
+        //full_name = map['full_name'],
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
 }
